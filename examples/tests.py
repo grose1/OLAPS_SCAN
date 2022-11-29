3a242eb2d58a02f49a7d83ecdb048bffd0c02fd5
grant <grantrose1@gmail.com>
2022-11-29 16:25:13 -0500

    1

--- examples/tests.py
+++ examples/tests.py
@@ -10,7 +10,15 @@ scanner.scan(ip_addr, '78-82', '-v -sS -sV -sC -A -O')
 print(scanner.scaninfo())
 print("Ip Status: ", scanner[ip_addr].state())
 print(scanner[ip_addr].all_protocols())
+os = scanner[ip_addr]['osmatch'][0]['name']
+accuracy = scanner[ip_addr]['osmatch'][0]['accuracy']
+host = scanner[ip_addr]['hostnames'][0]['name']
+print("OS: ", os)
+print("Accuracy: ", accuracy)
+print("Host: ", host)
+print("Open Ports: ", scanner[ip_addr].keys())
 for key, value in scanner[ip_addr]['tcp'].items():
+
     j = key
     if value['state'] == 'open':
         print('Open Ports: ', j)
